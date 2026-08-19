0c50652449734a388af95fb11e9d754d3058cd3e
onesip <info@onesip.nl>
2026-08-20 01:50:29 +0200

    Load V11 recognizer

--- recognition-v9.js
+++ recognition-v9.js
@@ -1,7 +1,7 @@
-// V10 loader. Previous recognition layers are disabled to avoid cascading mutations.
+// V11 loader. Previous recognition layers remain disabled to avoid cascading mutations.
 (() => {
   const s=document.createElement('script');
-  s.src='./recognition-v10.js?v=10';
+  s.src='./recognition-v11.js?v=11';
   s.defer=true;
   document.head.appendChild(s);
 })();
