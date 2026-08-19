6b17e2de2d5ba677f09fd6f44c1e8510533c3f39
onesip <info@onesip.nl>
2026-08-20 00:37:00 +0200

    Refresh app cache after calibration update

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
-const CACHE='pindou-v3';
+const CACHE='pindou-v4';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
-    const copy=res.clone();
-    if(new URL(e.request.url).origin===location.origin)caches.open(CACHE).then(c=>c.put(e.request,copy));
-    return res;
-  }).catch(()=>caches.match('./index.html'))));
+  const url=new URL(e.request.url);
+  if(url.origin===location.origin&&(url.pathname.endsWith('.html')||url.pathname.endsWith('.js')||url.pathname.endsWith('.css')||url.pathname.endsWith('/'))){
+    e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>caches.match(e.request).then(hit=>hit||caches.match('./index.html'))));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();if(url.origin===location.origin)caches.open(CACHE).then(c=>c.put(e.request,copy));return res;})));
 });
