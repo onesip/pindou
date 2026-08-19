ff300d2fb0bb16efc71a6c855291cc2f015dde94
onesip <info@onesip.nl>
2026-08-20 01:19:08 +0200

    Bump Pindou cache to V7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pindou-v6';
-const SHELL=['./','./index.html','./styles.css','./app.js','./ocr-patch.js','./mard-colors.js','./recognition-v6.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='pindou-v7';
+const SHELL=['./','./index.html','./styles.css','./app.js','./ocr-patch.js','./mard-colors.js','./recognition-v6.js','./recognition-v5.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
